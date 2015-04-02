7578fdd74feae3f6cc0f96c70a94e04aa00d4453
Ben Brittain <ben@brittain.org>
2015-04-01 20:29:13 -0400

    add route post route

--- src/petrarch/core.clj
+++ src/petrarch/core.clj
@@ -5,7 +5,7 @@
             [ring.middleware.edn :as edn]
             [compojure.route :as route]
             [compojure.handler :refer [site]]
-            [compojure.core :refer [defroutes GET POST DELETE ANY context]])
+            [compojure.core :refer [defroutes GET POST PUT DELETE ANY context]])
   (:gen-class))
 
 
@@ -22,15 +22,22 @@
 (defn get-entry [id]
   (let [entries (read-string (slurp "resources/data/entries.edn"))
         entry (first (filter #(= (str (:id %)) id) entries))]
-    (do
-      (generate-response {:text (:text entry)}))))
+    (generate-response {:text (:text entry)})))
+
+(defn get-routes []
+  (let [routes (read-string (slurp "resources/data/routes.edn"))]
+    (generate-response {:path routes})))
 
 (defroutes routes
   (GET "/" [] (resp/resource-response "index.html" {:root "public"}))
   (GET "/api/entry/" [] (get-entries-index))
   (GET "/api/entry/:id" [id] (get-entry id))
+  (POST "/api/routes/" [] (fn [req]
+                            (let [edn (:params req)]
+                              ;(save-coords (:coords edn))
+                              (generate-response {:text "good"}))))
   (route/resources "/")
-  (route/not-found "<p>No such adventure has been taken yet</p>")) ;; all other, return 404
+  (route/not-found "<p>No such adventure has been taken yet</p>"))
 
 ; App Setup
 
